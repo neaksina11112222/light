526ca89f2ac582e20cda9cbeaa62938cf6f71997
neaksina11112222 <neaksina752@gmail.com>
2024-09-17 14:49:41 +0700

    Add files via upload

---
+++ light/app.js
@@ -0,0 +1,16 @@
+// let btn = document.querySelector(".btn");
+// let body = document.body;
+// let audio = document.querySelector("#audio");
+
+// btn.addEventListener("click", () => {
+//   body.classList.toggle("on");
+//   audio.play();
+// });
+let btn = document.querySelector('.btn');
+let body = document.body;
+let audio = document.querySelector('#audio');
+
+btn.addEventListener('click', () => {
+  body.classList.toggle('on');
+  audio.play();
+  });
